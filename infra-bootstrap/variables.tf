dde6bee32c83e92c7bca44fed4d7bde926e906d4
chalalaz <beerkkc@gmail.com>
2023-03-05 00:19:26 +0700

    remove non relate

--- infra-bootstrap/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cidr_block" {
-  description = "CIDR Block to allow Site Access"
-  default = "0.0.0.0/0"
-}
-
-variable "key_name" {
-  description = "Name of keypair to ssh"
-  default = ""
-}
